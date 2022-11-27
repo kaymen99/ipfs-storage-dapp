910fc62864d1fd17115dd8e081c801493aceda57
Aymen <AymenMir1001@gmail.com>
2022-11-27 01:49:56 +0100

    Update NavbarElements.js

--- front-end/src/components/NavbarElements.js
+++ front-end/src/components/NavbarElements.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Nav = styled.nav`
   background: #000;
@@ -11,18 +11,13 @@ export const Nav = styled.nav`
 `;
 
 export const Title = styled.h1`
-color: #fff;
-align-text: left;
-
+  color: #fff;
+  align-text: left;
 `;
 
 export const Note = styled.div`
-color: #fff;
-align-text: center;
-font-size: 12px;
-margin-left: 15%;
-
-
+  color: #fff;
+  align-text: center;
+  font-size: 12px;
+  padding-top: 10px;
 `;
-
-
