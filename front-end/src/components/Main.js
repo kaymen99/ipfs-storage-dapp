898415894df255e2cae13c51327dc02064fa5216
Aymen <AymenMir1001@gmail.com>
2023-05-18 02:42:23 +0100

    Update Main.js

--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -1,47 +1,43 @@
-import React from 'react'
-import { useSelector } from "react-redux"
-import Account from "./Account"
-import FileStorage from "./FileStorage"
-
-import { Nav, Title, Note } from "./NavbarElements"
-
-
+import "../styles/index.css";
+import React from "react";
+import { useSelector } from "react-redux";
+import Account from "./Account";
+import FileStorage from "./FileStorage";
 
 function Main() {
-    const data = useSelector((state) => state.blockchain.value)
+  const data = useSelector((state) => state.blockchain.value);
+  const isConnected = data.account !== "";
 
-    const isConnected = data.account !== ""
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-content">
+          <h1 className="title">DStore</h1>
+          <div className="note">
+            {isConnected ? (
+              <p>
+                Note: You are currently connected to the{" "}
+                {data.network ? data.network : "unknown"} network
+              </p>
+            ) : (
+              <p>Please connect your wallet</p>
+            )}
+          </div>
+          <Account />
+        </div>
+      </nav>
 
-    return (
-        <>
-            <Nav >
-                <Title>DStore</Title>
-                <Note >
-                    {isConnected ? (
-                        <p>
-                            Note: You are currently connected to the {data.network ? data.network : "unknown"} network
-                        </p>
-                    ) : (
-                        <p>Please connect your wallet</p>
-                    )}
-                </Note>
-                <Account />
-            </Nav>
-
-            <h1>IPFS Storage Dapp</h1>
-            <br />
-            <div>
-
-                {isConnected ? (
-                    <>
-                        <FileStorage />
-                    </>
-                ) : null}
-
-            </div>
-
-        </>
-    )
+      <h1>IPFS Storage Dapp</h1>
+      <br />
+      <div>
+        {isConnected ? (
+          <>
+            <FileStorage />
+          </>
+        ) : null}
+      </div>
+    </>
+  );
 }
 
-export default Main
+export default Main;
