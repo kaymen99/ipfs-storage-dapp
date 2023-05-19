3c2a8d5078a0311d7fa6c95ddb0594cb19a85ae8
Aymen <AymenMir1001@gmail.com>
2023-05-19 01:37:49 +0100

    Update StoreContent.js

--- front-end/src/utils/StoreContent.js
+++ front-end/src/utils/StoreContent.js
@@ -12,10 +12,18 @@ function MakeStorageClient() {
   return new Web3Storage({ token: GetAccessToken() });
 }
 
-export const StoreContent = async (files) => {
+export const StoreContent = async (content) => {
+  console.log("Uploading content to IPFS with web3.storage....");
+  const client = MakeStorageClient();
+  const cid = await client.put([content]);
+  console.log("Stored content with cid:", cid);
+  return cid;
+};
+
+export const StoreManyFiles = async (files) => {
   console.log("Uploading files to IPFS with web3.storage....");
   const client = MakeStorageClient();
-  const cid = await client.put([files]);
+  const cid = await client.put(files);
   console.log("Stored files with cid:", cid);
   return cid;
 };
